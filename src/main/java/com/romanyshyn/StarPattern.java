e802182116eacf1d5cc9a32f4c68f7ebc0757ec1
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-07-09 21:38:38 -0800

    Training the cycle by solving star patterns

---
+++ src/main/java/com/romanyshyn/StarPattern.java
@@ -0,0 +1,13 @@
+package com.romanyshyn;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++){
+            System.out.println(" * ");
+            for (int j = 1; j <= i; j++) {
+                System.out.println(" ");
+            }
+        }
+    }
+}
+
